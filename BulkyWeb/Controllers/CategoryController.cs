ceed7860096177960494b31bcc0d9ded26a3aac8
DalatCoder <hieuntctk42@gmail.com>
2025-07-26 21:20:04 +0700

    display all categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,6 +17,6 @@ public class CategoryController : Controller
     public IActionResult Index()
     {
         List<Category> categories = _db.Categories.ToList();
-        return View();
+        return View(categories);
     }
 }
