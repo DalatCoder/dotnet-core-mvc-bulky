f5f43faf4cdb8fbe49514a093d1b203f1f2df225
DalatCoder <hieuntctk42@gmail.com>
2025-07-27 16:00:59 +0700

    implement repository pattern

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,24 @@
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository.IRepository;
+
+public interface IRepository<T> where T : class
+{
+    // T có thể là Category hoặc bất kỳ generic model nào khác
+    // để thực hiện các thao tác CRUD hoặc tương tác với DB context
+    
+    // Lấy tất cả bản ghi
+    IEnumerable<T> GetAll();
+    
+    // Lấy một bản ghi dựa trên điều kiện LINQ
+    T Get(Expression<Func<T, bool>> filter);
+    
+    // Thêm một entity
+    void Add(T entity);
+    
+    // Xóa một entity
+    void Remove(T entity);
+    
+    // Xóa nhiều entities cùng lúc
+    void RemoveRange(IEnumerable<T> entities);
+}
