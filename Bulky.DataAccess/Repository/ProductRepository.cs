3d8c82c7604dbae1abcad3f2f0a91272cf3dd99a
DalatCoder <hieuntctk42@gmail.com>
2025-07-29 07:30:38 +0700

    manual mapping for updating

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -6,7 +6,7 @@ namespace Bulky.DataAccess.Repository.IRepository;
 public class ProductRepository : Repository<Product>, IProductRepository
 {
     private readonly ApplicationDbContext _db;
-    
+
     public ProductRepository(ApplicationDbContext db) : base(db)
     {
         _db = db;
@@ -14,6 +14,24 @@ public class ProductRepository : Repository<Product>, IProductRepository
 
     public void Update(Product product)
     {
-        _db.Products.Update(product);
+        var productFromDb = _db.Products.FirstOrDefault(u => u.Id == product.Id);
+
+        if (productFromDb != null)
+        {
+            productFromDb.Title = product.Title;
+            productFromDb.ISBN = product.ISBN;
+            productFromDb.Price = product.Price;
+            productFromDb.Price50 = product.Price50;
+            productFromDb.Price100 = product.Price100;
+            productFromDb.ListPrice = product.ListPrice;
+            productFromDb.Description = product.Description;
+            productFromDb.CategoryId = product.CategoryId;
+            productFromDb.Author = product.Author;
+
+            if (product.ImageUrl != null)
+            {
+                productFromDb.ImageUrl = product.ImageUrl;
+            }
+        }
     }
 }
